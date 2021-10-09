e76d33c8fe7be47a5f65f1f43934395454ea3896
Bree <breemars@knights.ucf.edu>
2021-10-09 17:57:16 -0400

    41ps

---
+++ exercise41/src/main/java/Solution41.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution41 {
+    /*
+    Sorting Names from a File
+    - Read names from the input file
+    - store all the names in an arraylist
+    - sort the array list alphabetically
+    - print the arraylist into a new output file
+    */
+    public static void main(String[] args) throws IOException {
+        //Read data into a list of strings
+
+        //Sort data by last name
+
+        //Write new list to file
+    }
+
+    //Read data from file into list
+    private static List<String> readData() throws FileNotFoundException {
+        }
+
+    //Write new data from list to file
+    private static void writeData(List<String> names) throws IOException {
+       }
+
+}
