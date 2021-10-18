4fc253be04c93a2e8596f9a9450f9f8883aaa760
Bree <breemars@knights.ucf.edu>
2021-10-17 22:09:40 -0400

    45ps

---
+++ exercise45/src/main/java/Solution45/Solution45.java
@@ -0,0 +1,40 @@
+package Solution45;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution45 {
+    /*
+    Word Finder
+    - get the name out output file from user
+    - import the input file line by line
+    - search for the search word in each line
+    - replace with the new word if found
+    - store the new line in a list of strings
+    - output the finished list of strings to a new file named by the user
+    */
+    public static void main(String[] args) throws IOException {
+
+        //Get output file name
+
+        //Create new file
+        }
+
+    //Switch the words and read data from file into list
+    public static List<String> readData(String search, String replace) throws IOException {
+
+            //If word found, replace in the line
+ 
+    }
+
+    //Write new data from list to file
+    public static void writeData(List<String> data, String output) throws IOException {
+
+
+    }
+
+}
---
+++ exercise45/src/test/java/Solution45Test.java
@@ -0,0 +1,33 @@
+import Solution44.Solution44;
+import Solution44.Products;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+class Solution45Test {
+    List<String> data;
+    List<Products> products;
+
+    @BeforeEach @Test
+    void testReadingInData() throws IOException {
+        data = Solution44.readData();
+        assertEquals("    {\"name\": \"Widget\", \"price\": 25.00, \"quantity\": 5 }", data.get(0));
+
+    }
+
+    @BeforeEach @Test
+    void testCreateObjectList() {
+       products = Solution44.objectList(data);
+       assertEquals("Widget", products.get(0).getName());
+    }
+
+    @Test
+    void testSearchingForProduct() {
+        assertTrue(Solution44.searchProduct(products, "Doodad"));
+    }
+
+}
