3816ef689f7304597039877751bf73f2d1202cd8
Bree <breemars@knights.ucf.edu>
2021-10-17 20:51:20 -0400

    44ps

---
+++ exercise44/src/main/java/Solution44/Products.java
@@ -0,0 +1,20 @@
+package Solution44;
+
+public class Products {
+    private String name;
+    private double price;
+    private int quantity;
+
+    Products(String name, double price, int quantity){}
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+}
---
+++ exercise44/src/main/java/Solution44/Solution44.java
@@ -0,0 +1,47 @@
+package Solution44;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution44 {
+    /*
+    Product Search and JSON
+    - read product data from JSON file into a list of strings
+    - turn list of strings to list of objects using GSON
+    - prompt user for a product name
+    - search for product in list of objects
+    - if found, print product info and exit
+    - if not, keep asking for a name until one found
+    */
+    public static void main(String[] args) throws IOException {
+
+        //Get data from json file into a list of objects
+
+        //Ask user for product name and search for it
+
+    }
+
+   //Turn list of strings to list of objects using gson
+   public static List<Products> objectList(List<String> data){
+
+   }
+
+   //Search for a product in list of objects, print when found
+   public static boolean searchProduct(List<Products> productsList, String name) {
+
+   }
+
+    //Read data from json file into a list of strings
+    public static List<String> readData() throws IOException {
+       
+    }
+
+
+
+}
